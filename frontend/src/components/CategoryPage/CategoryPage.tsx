940e7a964b128fa65a5834a8d04669bb41c7762a
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-02-02 11:05:53 +0300

    fix: исправил страничку категорий товаров

--- frontend/src/components/CategoryPage/CategoryPage.tsx
+++ frontend/src/components/CategoryPage/CategoryPage.tsx
@@ -1,12 +1,19 @@
 import { observer } from "mobx-react-lite"
 import { useParams } from "react-router-dom"
+import styled from "styled-components"
 import pizzaStore from "../../mobX/pizzaStore"
 import Item from "../Item/Item"
 
 const CategoryPage = () => {
     const params = useParams().name
 
-    return (<div>{pizzaStore.products.find((grup) => grup.name == params)!.items.map(item => <Item {...item} />)}</div>)
+    return (<Grid>{pizzaStore.products.find((grup) => grup.name == params)!.items.map(item => <Item {...item} />)}</Grid>)
 }
+
+const Grid = styled.div`
+    display: grid;
+    gap: 16px;
+    grid-template-columns: 1fr 1fr 1fr;
+`
 
 export default observer(CategoryPage)
