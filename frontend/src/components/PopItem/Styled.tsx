7b0aeb2a38bbbfb649e7673db9d02d62c4b4bbc5
Sergey-SM2021 <84789763+Sergey-SM2021@users.noreply.github.com>
2022-02-02 15:06:31 +0300

    Рефакторинг

---
+++ frontend/src/components/PopItem/Styled.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+export const Modal = styled.div`
+    background: #ffffff;
+    width: 900px;
+    height: 470px;
+    border-radius: 8px;
+`
+
+export const Pop = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    position:fixed;
+    top: 0px;
+    left: 0px;
+    background: #0000008b;
+    backdrop-filter: blur(8px);
+`
